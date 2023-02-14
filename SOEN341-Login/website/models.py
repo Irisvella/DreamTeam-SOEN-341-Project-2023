fc3044f26bd0bb215d7f3d82dda81fe010691ab3
Irisvella <122749049+Irisvella@users.noreply.github.com>
2023-02-13 20:20:10 -0500

    Update models.py
    typo correction and job seeker model update

--- SOEN341-Login/website/models.py
+++ SOEN341-Login/website/models.py
@@ -15,7 +15,7 @@ class Employer(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), unique=True)
     company_name = db.Column(db.String(50), unique=True)
-    passowrd = db.Column(db.String(50))
+    password = db.Column(db.String(50))
 
 #model for a job seeker 
 class User_seeker(db.Model, UserMixin): 
@@ -23,4 +23,6 @@ class User_seeker(db.Model, UserMixin):
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(20))
     first_name = db.Column(db.String(50))
+    last_name = db.Column(db.String(50))
+    phone_number = db.Column(db.String(15))
     notes = db.relationship('Note') #everytime a note is created, the note id is stored here.
