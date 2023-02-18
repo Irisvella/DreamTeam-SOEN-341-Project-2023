ba5ee9453aad1b9eecbd608817451785a5c31875
Fatoumatabintabarry <97992954+Fatoumatabintabarry@users.noreply.github.com>
2023-02-18 11:49:18 -0500

    fixed again logout functionality + added d menu elements and pages + did the job posting page and created a post page

--- SOEN341-Login/website/views.py
+++ SOEN341-Login/website/views.py
@@ -2,25 +2,27 @@
 from flask import Blueprint, render_template, request,flash, redirect, url_for
 from flask_login import login_required, current_user
 from flask import Flask, render_template
-from .models import Post
+from .models import Post, User
 from. import db
 
 
 views = Blueprint('views', __name__)
 
 @views.route('/') #insert URL here
-@views.route('/home') 
+@views.route("/home")
 @login_required
 
-
 def home():
-    posts = Post.query.all()
-    return render_template("home.html", user=current_user, posts=posts)  #renders the HTML inside the home.html file
+    return render_template("home.html",user=current_user)
+
 
 @views.route('/seeker_home')
 @login_required
 def seeker_home():
-    return render_template("seeker_home.html", user=current_user)
+    posts = Post.query.all()
+    print(posts)
+    return render_template("seeker_home.html", user=current_user, posts=posts)
+
 
 @views.route('/employer_home')
 @login_required
@@ -31,20 +33,55 @@ def employer_home():
 def about():
     return render_template('about.html', user=current_user)
 
+
+@views.route('/testimonials')
+def testimonials():
+    return render_template('testimonials.html', user=current_user)
+
+
 @views.route("/create-post", methods=['GET','POST'])
 @login_required
 def create_post():
     if request.method == "POST":
-        text= request.form.get('text')
+        text = request.form.get('text')
 
         if not text:
-            flash('Post cannot be empty', category ='error')
+            flash('Post cannot be empty', category='error')
         else:
             post = Post(text=text, author=current_user.id)
             db.session.add(post)
             db.session.commit()
-            flash('Post created',category ='success')
+            flash('Post created!', category='success')
             return redirect(url_for('views.home'))
 
-    return render_template('create_post.html',user=current_user)
+    return render_template('create_post.html', user=current_user)
+
+@views.route("/delete-post/<id>")
+@login_required
+def delete_post(id):
+    post = Post.query.filter_by(id=id).first()
+
+    if not post:
+        flash ("Post does not exist.", category='error')
+    elif current_user.id != post.id:
+        flash ('you do not have permission to delete this post.',category='error')
+    else:
+        db.session.delete(post)
+        db.session.commit()
+        flash('Post deleted', category='success')
+        
+    return redirect (url_for('views.home'))
+
+@views.route("/posts/<username>")
+@login_required
+def posts(username):
+    user = user.query.filter_by(username=username).first()
+
+    if not user:
+        flash('No user wih that username exists.', category='error')
+        return redirect(url_for('views.home'))
+
+    post = Post.query.filter_by(author=user.id).all()
+    return render_template("posts.html", user=current_user, posts= posts,username=username )
+
 
