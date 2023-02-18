237b8e49a6ce4938ceb25bc42009a4d136c42dc8
Irisvella <122749049+Irisvella@users.noreply.github.com>
2023-02-18 13:06:17 -0500

    Revert "Merge branch 'Sprint-2-login-system' of https://github.com/Irisvella/DreamTeam-SOEN-341-Project-2023 into Sprint-2-login-system"
    This reverts commit f04c8c31e20d686910f1c081106671723578be39, reversing
    changes made to b7b57292c7ca10b17ad5376024630da938a1fc87.

--- SOEN341-Login/website/auth.py
+++ SOEN341-Login/website/auth.py
@@ -1,31 +1,33 @@
-from flask import Blueprint, render_template, redirect, url_for, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User, Employer
+from werkzeug.security import generate_password_hash, check_password_hash    #securing passwords 
 from . import db
-from .models import User
-from flask_login import login_user, logout_user, login_required, current_user
-from werkzeug.security import generate_password_hash, check_password_hash
-
-auth = Blueprint("auth", __name__)
+from flask_login import login_required, login_user, logout_user, current_user
 
 
-@auth.route("/login", methods=['GET', 'POST'])
+auth = Blueprint('auth', __name__)
+
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    if request.method == 'POST':
-        email = request.form.get("email")
-        password = request.form.get("password")
+    if request.method =='POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
 
+        #search db by a specific field. Login process to check what user entered with whats in the db
         user = User.query.filter_by(email=email).first()
         if user:
-            if check_password_hash(user.password, password):
-                flash("Logged in!", category='success')
-                login_user(user, remember=True)
-                return redirect(url_for('views.home'))
-            else:
-                flash('Password is incorrect.', category='error')
+            if check_password_hash(user.password,password):
+                flash('Logged in successfully.', category='success')
+                login_user(user, remember=True) #from flask_login, remembers that the user is logged in. Stored in session. 
+                if request.form.get('profile') == "seeker":
+                    return redirect(url_for('views.seeker_home')) #redirect to the page 
+                if request.form.get('profile') == "employer":
+                    return redirect(url_for('views.employer_home'))
+            else: 
+                flash('Incorrect password.', category='error')
         else:
-            flash('Email does not exist.', category='error')
-
+            flash('Email does not exist', category='error')
     return render_template("login.html", user=current_user)
-
 
 @auth.route('/logout')
 @login_required #can only access the logout function if a user is logged in. 
@@ -36,44 +38,63 @@ def logout():
     #return render_template("auth.login")
     return render_template("login.html", user = current_user)
 
-
-@auth.route("/sign-up", methods=['GET', 'POST'])
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
-    if request.method == 'POST':
-        email = request.form.get("email")
-        username = request.form.get("username")
-        password1 = request.form.get("password1")
-        password2 = request.form.get("password2")
+    if request.method == 'POST': 
+        email = request.form.get('email')
+        first_name = request.form.get('first_name')
+        last_name = request.form.get('last_name')
+        phone_number = request.form.get('phone_number')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
 
-        email_exists = User.query.filter_by(email=email).first()
-        username_exists = User.query.filter_by(username=username).first()
+        #checks if the email is already in use 
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash('Email already exists', category='error')
 
-        if email_exists:
-            flash('Email is already in use.', category='error')
-        elif username_exists:
-            flash('Username is already in use.', category='error')
+        if len(email) < 2:
+            flash('Email must be greater than 4 characters.', category='error') #flashes error message
+        elif len(first_name) < 2:
+            flash('First name must be greater than 4 characters.', category='error')
         elif password1 != password2:
-            flash('Password don\'t match!', category='error')
-        elif len(username) < 2:
-            flash('Username is too short.', category='error')
-        elif len(password1) < 6:
-            flash('Password is too short.', category='error')
-        elif len(email) < 4:
-            flash("Email is invalid.", category='error')
+            flash('Passwords must match.', category='error')
+        elif len(password1) < 1:
+            flash('Password must be greater than 6 characters.', category='error')
         else:
-            new_user = User(email=email, username=username, password=generate_password_hash(
-                password1, method='sha256'))
-            db.session.add(new_user)
+            user = User(profile="seeker",email=email, first_name=first_name, last_name=last_name, phone_number = phone_number, password=generate_password_hash(password1, method='sha256'))
+            db.session.add(user)
             db.session.commit()
-            login_user(new_user, remember=True)
-            flash('User created!')
-            return redirect(url_for('views.home'))
-
+            login_user(user, remember=True) #from flask_login, remembers that the user is logged in. Stored in session. 
+            flash('Account created successfully.', category='success')
+            return redirect(url_for('views.seeker_home'))
     return render_template("signup.html", user=current_user)
 
+@auth.route('/signup_employer', methods=['GET', 'POST'])
+def signup_employer():
+    if request.method == 'POST': 
+        email = request.form.get('email')
+        company_name = request.form.get('company_name')
+        phone_number = request.form.get('phone_number')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
 
-@auth.route("/logout")
-@login_required
-def logout():
-    logout_user()
-    return redirect(url_for("views.home"))
+        #checks if the email is already in use 
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash('Email already exists', category='error')
+
+        if len(email) < 2:
+            flash('Email must be greater than 4 characters.', category='error') #flashes error message
+        elif password1 != password2:
+            flash('Passwords must match.', category='error')
+        elif len(password1) < 1:
+            flash('Password must be greater than 6 characters.', category='error')
+        else:
+            user = User(profile="employer",email=email, company_name=company_name, phone_number = phone_number, password=generate_password_hash(password1, method='sha256'))
+            db.session.add(user)
+            db.session.commit()
+            login_user(user, remember=True) #from flask_login, remembers that the user is logged in. Stored in session. 
+            flash('Account created successfully.', category='success')
+            return redirect(url_for('views.employer_home'))
+    return render_template("signup_employer.html", user=current_user)
--- SOEN341-Login/website/views.py
+++ SOEN341-Login/website/views.py
@@ -2,27 +2,25 @@
 from flask import Blueprint, render_template, request,flash, redirect, url_for
 from flask_login import login_required, current_user
 from flask import Flask, render_template
-from .models import Post, User
+from .models import Post
 from. import db
 
 
 views = Blueprint('views', __name__)
 
 @views.route('/') #insert URL here
-@views.route("/home")
+@views.route('/home') 
 @login_required
 
-def home():
-    return render_template("home.html",user=current_user)
 
+def home():
+    posts = Post.query.all()
+    return render_template("home.html", user=current_user, posts=posts)  #renders the HTML inside the home.html file
 
 @views.route('/seeker_home')
 @login_required
 def seeker_home():
-    posts = Post.query.all()
-    print(posts)
-    return render_template("seeker_home.html", user=current_user, posts=posts)
-
+    return render_template("seeker_home.html", user=current_user)
 
 @views.route('/employer_home')
 @login_required
@@ -33,55 +31,20 @@ def employer_home():
 def about():
     return render_template('about.html', user=current_user)
 
-
-@views.route('/testimonials')
-def testimonials():
-    return render_template('testimonials.html', user=current_user)
-
-
-@views.route("/create-post", methods=['GET','POST'])
+@views.route("/create_post", methods=['GET','POST'])
 @login_required
 def create_post():
     if request.method == "POST":
-        text = request.form.get('text')
+        text= request.form.get('text')
 
         if not text:
-            flash('Post cannot be empty', category='error')
+            flash('Post cannot be empty', category ='error')
         else:
             post = Post(text=text, author=current_user.id)
             db.session.add(post)
             db.session.commit()
-            flash('Post created!', category='success')
+            flash('Post created',category ='success')
             return redirect(url_for('views.home'))
 
-    return render_template('create_post.html', user=current_user)
-
-@views.route("/delete-post/<id>")
-@login_required
-def delete_post(id):
-    post = Post.query.filter_by(id=id).first()
-
-    if not post:
-        flash ("Post does not exist.", category='error')
-    elif current_user.id != post.id:
-        flash ('you do not have permission to delete this post.',category='error')
-    else:
-        db.session.delete(post)
-        db.session.commit()
-        flash('Post deleted', category='success')
-        
-    return redirect (url_for('views.home'))
-
-@views.route("/posts/<username>")
-@login_required
-def posts(username):
-    user = user.query.filter_by(username=username).first()
-
-    if not user:
-        flash('No user wih that username exists.', category='error')
-        return redirect(url_for('views.home'))
-
-    post = Post.query.filter_by(author=user.id).all()
-    return render_template("posts.html", user=current_user, posts= posts,username=username )
-
+    return render_template('create_post.html',user=current_user)
 
